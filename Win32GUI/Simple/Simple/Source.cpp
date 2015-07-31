3d7ab988f61c000d695ff8316435b543e02a0db3
kingnerdthethird <kingnerdthethird@outlook.com>
2015-07-31 09:08:49 -0600

    Backup

---
+++ Win32GUI/Simple/Simple/Source.cpp
@@ -0,0 +1,6 @@
+#include <windows.h>
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow){
+	MessageBox(NULL, "Hello, World!", "Simple Message", MB_OK);
+	return 0;
+}
